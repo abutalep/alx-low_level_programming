e386bc0e303d86d408d544806217442e49d84a6a
myname <myemail@gmail.com>
2023-04-03 09:53:38 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets pointer val to char
+ *
+ * @s: string double pointer
+ * @to: string pointer
+ **/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
