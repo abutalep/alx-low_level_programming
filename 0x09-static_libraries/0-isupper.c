4d6870608471e8c7f27484729aca45687b23d9ed
myname <myemail@gmail.com>
2023-04-11 05:40:25 -0700

    libmy

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,15 @@
-#include "main.h"
-
 /**
- * _isupper - check uppercase alphabets
- * @c: character
- * Return: 1 if c is uppercase otherwise 0.
- **/
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
+
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-else
-return (0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,16 +1,18 @@
-#include "stdio.h"
+#include"main.h"
 
 /**
- * _islower - check lowercase alphabets
- * @c: letter input by user
- * Return: 1 if c is lowercase othrwise 0.
- **/
+ * _islower - function to check if
+ *           character is lowercase
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if `c` is lowercase
+ *         otherwise always 0 (Success)
+*/
 
 int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-return (1);
-else
-return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
 }
-
